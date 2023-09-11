2b4ba9eb12a2ca057ad336ad13e041419cb61712
skanishka2 <saikanishkakotha@gmail.com>
2023-09-11 12:28:02 -0400

    first  commit

---
+++ DemoApi/Controllers/InfoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    public class InfoController : ControllerBase
+    {
+        [HttpGet("/info")]
+        public async Task<ActionResult> GetTheInfo()
+        {
+            return Ok("its working");
+        }
+    }
+}
